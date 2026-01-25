09f48888f026d6402d229d5cbe0a18e0470b9c07
nizamjensani <nizam.jensani@newtycoon.com>
2026-01-25 08:14:36 +0800

    appearance dark hide

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,13 +28,13 @@
             @auth
             <a
                 href="{{ route('filament.admin.pages.dashboard') }}"
-                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                class="inline-block px-5 py-1.5  border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal">
                 Dashboard
             </a>
             @else
             <a
                 href="{{ route('filament.admin.auth.login') }}"
-                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
+                class="inline-block px-2 sm:px-5 py-1.5 text-[#1b1b18] border border-transparent hover:border-[#19140035] rounded-sm text-sm leading-normal">
                 Log in
             </a>
 
